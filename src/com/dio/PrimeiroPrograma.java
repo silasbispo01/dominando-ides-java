a42b3c3e6dde4191c1eb1ea8092abe04fb5be9fd
silasbispo01 <silasbispo01@gmail.com>
2021-07-27 11:08:01 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+	}
+
+}
